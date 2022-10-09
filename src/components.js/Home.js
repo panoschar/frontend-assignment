14c01d7164c33d97be94227af679362f2ed62732
panos-charalampopoulos <panos10cha@gmail.com>
2022-10-09 23:41:03 +0300

    attempt to fetch data

--- src/components.js/Home.js
+++ src/components.js/Home.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { useMemo } from "react";
+import { useMemo, useEffect } from "react";
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
-
-
+import axios from "axios";
 
 const mapContainerStyle = { width: "100%", height: "100vh", margin: "0" };
 
@@ -30,16 +29,57 @@ function Map() {
     </GoogleMap>;
 };
 
+// useEffect (() => {
+//     axios.get("https://services.marinetraffic.com/api/exportvesseltrack/{4edce672d943668d9f2201ddad602648151366c2}/").then(({data}) => {
+//      console.log(data)
+//     });
+//  });
+// function Search() {
+//     axios.get("https://services.marinetraffic.com/api/exportvesseltrack/{4edce672d943668d9f2201ddad602648151366c2}").then(({data}) => {
+//         console.log(data)
+//     })
+//     return <div></div>
+// }
+
+// const Data = () => {
+//     return axios.get("https://services.marinetraffic.com/api/exportvesseltrack/{4edce672d943668d9f2201ddad602648151366c2}")
+//           .then((response) => console.log(response));
+//   }
+
+
+// const Data = () => {
+         
+//     axios
+//       .get(
+//           `https://services.marinetraffic.com/api/exportvesseltrack/{4edce672d943668d9f2201ddad602648151366c2}`
+//       )
+//       .then((response) => {
+//           console.log(response.data);
+//       });
+//   }
+
+ 
+
 function Events() {
 
-    return (
+    const fetchData = () => {
+        return axios.get("https://services.marinetraffic.com/api/exportvesseltrack/{4edce672d943668d9f2201ddad602648151366c2}")
+              .then((data) => console.log(data));
+      }
+    
+      useEffect(() => {
+        fetchData();
+      },[])
+
+     return (
         <div className='box'>
             <h1>panos</h1>
             <input></input>
             <button>find vessel</button>
         </div>
     )
-}
+};
+
 
 
 
